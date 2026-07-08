dae240c750fd0bc7aadd0a2b414fa737188d7d32
BoarArtem <workboar@gmail.com>
2026-07-09 01:17:29 +0300

    adding some parameters for dataloader

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@ from torchvision import transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 
+normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                  std=[0.229, 0.224, 0.225])
 
 # data augmentation
 train_transform = transforms.Compose([
@@ -13,12 +15,14 @@ train_transform = transforms.Compose([
         contrast=0.3,
         saturation=0.3
     ),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    normalize
 ])
 
 test_transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    normalize
 ])
 
 # dataset exc.
@@ -26,5 +30,5 @@ train_dataset = ImageFolder(root="../data/seg_train/seg_train", transform=train_
 test_dataset = ImageFolder(root="../data/seg_test/seg_test", transform=test_transform)
 
 # dataloader
-train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=64)
+train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True, num_workers=4, pin_memory=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=64, num_workers=4, pin_memory=True)
